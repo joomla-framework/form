e2caed15f9c7b569f4b164d6d58f97b0424b9a94
Don Gilbert <don@dongilbert.net>
2013-03-27 00:31:19 -0500

    Move vendor/Joomla to src/Joomla. Update necessary paths.

---
+++ Field/Language.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Part of the Joomla Framework Form Package
+ *
+ * @copyright  Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE
+ */
+
+namespace Joomla\Form;
+
+use Joomla\Html\Html;
+use Joomla\Language\Helper as LanguageHelper;
+
+/**
+ * Form Field class for the Joomla Platform.
+ * Supports a list of installed application languages
+ *
+ * @see    JFormFieldContentLanguage for a select list of content languages.
+ * @since  1.0
+ */
+class Field_Language extends Field_List
+{
+	/**
+	 * The form field type.
+	 *
+	 * @var    string
+	 * @since  1.0
+	 */
+	protected $type = 'Language';
+
+	/**
+	 * Method to get the field options.
+	 *
+	 * @return  array  The field option objects.
+	 *
+	 * @since   1.0
+	 */
+	protected function getOptions()
+	{
+		// Initialize some field attributes.
+		$client = (string) $this->element['client'];
+
+		if ($client != 'site' && $client != 'administrator')
+		{
+			$client = 'site';
+		}
+
+		// Merge any additional options in the XML definition.
+		$options = array_merge(
+			parent::getOptions(),
+			LanguageHelper::createLanguageList($this->value, constant('JPATH_' . strtoupper($client)), true, true)
+		);
+
+		return $options;
+	}
+}
---
+++ Field/Timezone.php
@@ -0,0 +1,103 @@
+<?php
+/**
+ * Part of the Joomla Framework Form Package
+ *
+ * @copyright  Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE
+ */
+
+namespace Joomla\Form;
+
+use Joomla\Factory;
+use Joomla\Html\Html;
+
+/**
+ * Form Field class for the Joomla Platform.
+ *
+ * @since  1.0
+ */
+class Field_Timezone extends Field_GroupedList
+{
+	/**
+	 * The form field type.
+	 *
+	 * @var    string
+	 * @since  1.0
+	 */
+	protected $type = 'Timezone';
+
+	/**
+	 * The list of available timezone groups to use.
+	 *
+	 * @var    array
+	 *
+	 * @since  1.0
+	 */
+	protected static $zones = array('Africa', 'America', 'Antarctica', 'Arctic', 'Asia', 'Atlantic', 'Australia', 'Europe', 'Indian', 'Pacific');
+
+	/**
+	 * Method to get the time zone field option groups.
+	 *
+	 * @return  array  The field option objects as a nested array in groups.
+	 *
+	 * @since   1.0
+	 */
+	protected function getGroups()
+	{
+		$groups = array();
+
+		$keyField = $this->element['key_field'] ? (string) $this->element['key_field'] : 'id';
+		$keyValue = $this->form->getValue($keyField);
+
+		// If the timezone is not set use the server setting.
+		if (strlen($this->value) == 0 && empty($keyValue))
+		{
+			$this->value = Factory::getConfig()->get('offset');
+		}
+
+		// Get the list of time zones from the server.
+		$zones = \DateTimeZone::listIdentifiers();
+
+		// Build the group lists.
+		foreach ($zones as $zone)
+		{
+			// Time zones not in a group we will ignore.
+			if (strpos($zone, '/') === false)
+			{
+				continue;
+			}
+
+			// Get the group/locale from the timezone.
+			list ($group, $locale) = explode('/', $zone, 2);
+
+			// Only use known groups.
+			if (in_array($group, self::$zones))
+			{
+				// Initialize the group if necessary.
+				if (!isset($groups[$group]))
+				{
+					$groups[$group] = array();
+				}
+
+				// Only add options where a locale exists.
+				if (!empty($locale))
+				{
+					$groups[$group][$zone] = Html::_('select.option', $zone, str_replace('_', ' ', $locale), 'value', 'text', false);
+				}
+			}
+		}
+
+		// Sort the group lists.
+		ksort($groups);
+
+		foreach ($groups as $zone => & $location)
+		{
+			sort($location);
+		}
+
+		// Merge any additional groups in the XML definition.
+		$groups = array_merge(parent::getGroups(), $groups);
+
+		return $groups;
+	}
+}
