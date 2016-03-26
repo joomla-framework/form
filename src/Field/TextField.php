80c16037a5c344b857ab0dc43492383613a1931b
photodude <photodude@users.noreply.github.com>
2016-03-26 16:35:44 -0400

    PHPCS fix (Fix #34)

--- src/Field/TextField.php
+++ src/Field/TextField.php
@@ -61,7 +61,8 @@ class TextField extends \Joomla\Form\Field
 		// Initialize JavaScript field attributes.
 		$onchange = $this->element['onchange'] ? ' onchange="' . (string) $this->element['onchange'] . '"' : '';
 
-		return '<input type="' . strtolower($this->type) . '" name="' . $this->name . '" id="' . $this->id . '"' . ' value="'
-			. htmlspecialchars($this->value, ENT_COMPAT, 'UTF-8') . '"' . $class . $size . $disabled . $readonly . $onchange . $maxLength . $placeholder . $autofocus . '/>';
+		return '<input type="' . strtolower($this->type) . '" name="' . $this->name . '" id="' . $this->id . '"'
+			. ' value="' . htmlspecialchars($this->value, ENT_COMPAT, 'UTF-8') . '"' . $class . $size . $disabled
+			. $readonly . $onchange . $maxLength . $placeholder . $autofocus . '/>';
 	}
 }
