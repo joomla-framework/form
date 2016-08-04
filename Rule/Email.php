62811cdf83371048577d28899c40c1af16323ea5
Michael Babker <mbabker@flbab.com>
2016-08-04 15:09:55 -0500

    Backport joomla/joomla-cms@98c48eee875c1953ec011f9dc8ea4ac58563dd2a

--- Rule/Email.php
+++ Rule/Email.php
@@ -59,7 +59,8 @@ class Email extends Rule
 
 		if ($tld)
 		{
-			$this->regex = '^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]{2,})$';
+			$this->regex = '^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])'
+				. '?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$';
 		}
 
 		// Determine if the multiple attribute is present
